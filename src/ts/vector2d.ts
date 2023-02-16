a9cfccdd11cbcd2438a67761ae2906696cc17f7a
martijn v. Arkel <cmvanarkel@hotmail.com>
2023-02-16 21:05:14 +0100

    Added some basic math functions

--- src/ts/vector2d.ts
+++ src/ts/vector2d.ts
@@ -1,23 +1,36 @@
 export class Vector2D {
     constructor(public x = 0, public y = 0) { }
 
-    add(f: number): Vector2D { 
+    public add(f: number): Vector2D { 
         return new Vector2D(this.x + f, this.y + f);
     }
 
-    addVector(v: Vector2D): Vector2D { 
+    public addVector(v: Vector2D): Vector2D { 
         return new Vector2D(this.x + v.x, this.y + v.y);
     }
 
-    multiply(f: number) : Vector2D {
+    public multiply(f: number) : Vector2D {
         return new Vector2D(this.x * f, this.y * f);
     }
 
-    multiplyByVector2D(v: Vector2D) : Vector2D {
+    public multiplyByVector2D(v: Vector2D) : Vector2D {
         return new Vector2D(this.x * v.x, this.y * v.y);
     }
 
-    toString(): string {
+    public calculateLength(): number {
+        return Math.sqrt((this.x*this.x)+(this.y*this.y));
+    }
+
+    public dotProduct(v: Vector2D): number{
+        return this.x*v.x + this.y*v.y;
+    }
+
+    public getNomalized(): Vector2D{
+        const length = this.calculateLength();
+        return new Vector2D(this.x/length, this.y/length);
+    }
+
+    public  toString(): string {
         return `{ x: ${this.x}, y: ${this.y} }`;
     }
 }
